3ec84bcf9384899c82a137ec5077c79aefc82d86
Ziv <ziv.perry@zoominfo.com>
2021-05-05 23:45:06 +0300

    test: functions

--- lib/async-hooks-state.spec.ts
+++ lib/async-hooks-state.spec.ts
@@ -1,3 +1,17 @@
-describe('async-hooks-state', () => {
+import {getStorage, hooked} from './async-hooks-state';
+import {AsyncLocalStorage} from 'async_hooks';
 
+describe('async-hooks-state', () => {
+    it('should return AsyncLocalStorage object', () => {
+        expect(getStorage()).toBeInstanceOf(AsyncLocalStorage);
+    });
+
+    it('should return the same AsyncLocalStorage object', () => {
+        expect(getStorage()).toBe(getStorage());
+    });
+
+    it('should create a function', () => {
+        expect(hooked()).toBeInstanceOf(Function);
+        expect(hooked().length).toBe(3);
+    });
 });
