ce5fe53c60fb5af6ee6c214447a489465df7f0cf
Ziv <ziv.perry@zoominfo.com>
2021-05-09 12:15:20 +0300

    chore: api chages

--- lib/async-hooks-state.spec.ts
+++ lib/async-hooks-state.spec.ts
@@ -1,18 +1,71 @@
-import {getStorage, hooked} from './async-hooks-state';
-import {AsyncLocalStorage} from 'async_hooks';
+import {Context, getStorage, getStore, hooked} from './async-hooks-state';
+import {AsyncLocalStorage, executionAsyncId} from 'async_hooks';
 
 describe('async-hooks-state', () => {
-    it('should return AsyncLocalStorage object', () => {
-        expect(getStorage()).toBeInstanceOf(AsyncLocalStorage);
+    describe('construction', () => {
+        it('should return AsyncLocalStorage object', () => {
+            expect(getStorage()).toBeInstanceOf(AsyncLocalStorage);
+        });
+
+        it('should return the same AsyncLocalStorage object', () => {
+            expect(getStorage()).toBe(getStorage());
+        });
+
+        it('should create a function with 3 args (middleware)', () => {
+            const func = hooked();
+            expect(func).toBeInstanceOf(Function);
+            expect(func.length).toBe(3);
+        });
     });
 
-    it('should return the same AsyncLocalStorage object', () => {
-        expect(getStorage()).toBe(getStorage());
-    });
+    describe('async test', () => {
+        const asyncFunc = () => Promise.resolve(true);
 
-    it('should create a function with 3 args (middleware)', () => {
-        const func = hooked();
-        expect(func).toBeInstanceOf(Function);
-        expect(func.length).toBe(3);
+        it('should keep async data', done => {
+            const c1: Context = new Map([['test', 1], ['inc', 0]]);
+            const c2: Context = new Map([['test', 2], ['inc', 0]]);
+
+            const sync = {executor: executionAsyncId()};
+
+            const assertExecIdChanged = () => {
+                const executor = executionAsyncId();
+                expect(executor).not.toEqual(sync.executor);
+                sync.executor = executor;
+                return true;
+            };
+
+            const cb1 = () => Promise.resolve()
+                .then(() => assertExecIdChanged())
+                .then(() => {
+                    expect(getStore().get('test')).toBe(1);
+                    expect(getStore().get('inc')).toBe(0);
+                    getStore().set('inc', 1);
+                    return assertExecIdChanged();
+                }).then(() => {
+                    expect(getStore().get('test')).toBe(1);
+                    expect(getStore().get('inc')).toBe(1);
+                    return assertExecIdChanged();
+                });
+
+            const cb2 = () => Promise.resolve()
+                .then(() => assertExecIdChanged())
+                .then(() => {
+                    expect(getStore().get('test')).toBe(2);
+                    expect(getStore().get('inc')).toBe(0);
+                    getStore().set('inc', 2)
+                    return assertExecIdChanged();
+                }).then(() => {
+                    expect(getStore().get('test')).toBe(2);
+                    expect(getStore().get('inc')).toBe(2);
+                    return assertExecIdChanged();
+                });
+
+            Promise.all([
+                getStorage().run(c1, cb1),
+                getStorage().run(c2, cb2)
+            ]).then(() => {
+                done();
+            })
+        });
     });
 });
--- lib/async-hooks-state.ts
+++ lib/async-hooks-state.ts
@@ -1,17 +1,28 @@
 import {NextFunction, Request, Response} from 'express';
 import {AsyncLocalStorage as ALC} from 'async_hooks';
 
-export type ContextKey = string | symbol;
+export type ContextKey = number | string | symbol;
 export type Context = Map<ContextKey, any>;
 
 let storage: ALC<Context>;
+
 export const RequestSymbol = Symbol('request');
 export const ResponseSymbol = Symbol('response');
 
 
 export const getStorage = (): ALC<Context> => storage ? storage : storage = new ALC<Context>();
-export const getRequest = (): Request => getStorage().getStore().get(RequestSymbol);
-export const getResponse = (): Response => getStorage().getStore().get(ResponseSymbol);
+
+export const getStore = (): Context => {
+    const store = getStorage().getStore();
+    if (!store) {
+        throw new Error('Store not set yet. Make sure to register the hooked middleware.');
+    }
+    return store;
+}
+
+export const getRequest = (): Request => getStore().get(RequestSymbol);
+
+export const getResponse = (): Response => getStore().get(ResponseSymbol);
 
 export const hooked = (defaultContext: Record<string | number | symbol, unknown> = {}) =>
     (req: Request, res: Response, next: NextFunction) => getStorage().run(
